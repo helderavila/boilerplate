ef1a1f6c2086bf8bb7c64e17e1af1b839c0521ed
Helder Avila <helderavila2016@gmail.com>
2021-09-28 19:06:36 -0300

    Static files on storybook build

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,8 @@
 import * as S from './styles'
 
 const Main = ({
-  title = 'React Avançado',
-  description = 'TypeScript, ReactJS, NextJS e Styled Components',
+  title = 'NextJS Boilerplate',
+  description = 'TypeScript, NextJS e Styled Components',
 }) => (
   <S.Wrapper>
     <S.Logo
